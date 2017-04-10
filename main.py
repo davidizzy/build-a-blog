148ce812d0ed68f26e3a78777ab44e942864e3e0
davidizzy <davidizroy@gmail.com>
2017-04-10 14:54:02 -0500

    submit page completed pre-DB connection

--- main.py
+++ main.py
@@ -15,10 +15,44 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+class handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainHandler(handler):
+    def renderFront(self, title="", post="", error=""):
+        return self.render("newpost.html", title=title, post=post, error=error)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.renderFront()
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            self.write("Thanks!")
+        else:
+            error = "You need a title and a post!"
+            self.renderFront(title=title, post=post, error=error)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
